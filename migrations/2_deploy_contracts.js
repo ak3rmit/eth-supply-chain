fb9831ad55ee08b4e9fa9c48909f698070002af1
sealpuppy <adrianbeckdev@gmail.com>
2021-06-11 14:09:51 +0200

    Wrote supply chain Base

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,4 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var SimpleStorage = artifacts.require("./ItemManager.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
